f5a939f8c5e648f640b5f3dc296421a57b17ca51
John Pesavento <54077093+jpes707@users.noreply.github.com>
2021-07-15 17:57:09 -0400

    Day 4 complete

--- day04/final/script.js
+++ day04/final/script.js
@@ -1,51 +1,33 @@
 console.log("script running");
 
-// Fix these querySelectors so that they each select the correct element.
-const englishTile = document.querySelector("#English");
-const spanishTile = document.querySelector("#Spanish");
-const chineseTile = document.querySelector("#Chinese");
-const tagalogTile = document.querySelector("#Tagalog");
-const vietnameseTile = document.querySelector("#Vietnamese");
-const arabicTile = document.querySelector("#Arabic");
-const frenchTile = document.querySelector("#French");
-const koreanTile = document.querySelector("#Korean");
-const russianTile = document.querySelector("#Russian");
+// define list of languages
+const languages = [
+  "English",
+  "Spanish",
+  "Chinese",
+  "Tagalog",
+  "Vietnamese",
+  "Arabic",
+  "French",
+  "Korean",
+  "Russian",
+  "German",
+  "Haitian Creole",
+  "Hindi"
+];
 
-console.log(englishTile);
-console.log(spanishTile);
-console.log(chineseTile);
-console.log(tagalogTile);
-console.log(vietnameseTile);
-console.log(arabicTile);
-console.log(frenchTile);
-console.log(koreanTile);
-console.log(russianTile);
+// create an object for mapping languages to tiles
+let tileObject = {};
+languages.forEach((language) => {
+  tileObject[language.toLowerCase()] = document.querySelector(`#${language.replace(/\s/g, "")}`);
+});
 
-// Add the id of the input field so we can access it!
+// check a guessed language against the tiles and show tile if valid
 const inputField = document.querySelector("#guess");
-
-inputField.addEventListener("change", (e) => {
-  console.log("Guess submitted!");
-  let guess = inputField.value;
-  console.log(guess);
-  if (guess === "English" || guess === "english") {
-    englishTile.classList.remove("hidden");
-  } else if (guess === "Spanish" || guess === "spanish") {
-    spanishTile.classList.remove("hidden");
-  } else if (guess === "Chinese" || guess === "chinese") {
-    chineseTile.classList.remove("hidden");
-  } else if (guess === "Tagalog" || guess === "tagalog") {
-    tagalogTile.classList.remove("hidden");
-  } else if (guess === "Vietnamese" || guess === "vietnamese") {
-    vietnameseTile.classList.remove("hidden");
-  } else if (guess === "Arabic" || guess === "arabic") {
-    arabicTile.classList.remove("hidden");
-  } else if (guess === "French" || guess === "french") {
-    frenchTile.classList.remove("hidden");
-  } else if (guess === "Korean" || guess === "korean") {
-    koreanTile.classList.remove("hidden");
-  } else if (guess === "Russian" || guess === "russian") {
-    russianTile.classList.remove("hidden");
-  }
-  inputField.value = "";
+inputField.addEventListener("change", () => {
+    const guess = inputField.value.toLowerCase();
+    if (guess in tileObject) {
+      tileObject[guess].classList.remove("hidden");
+    }
+    inputField.value = "";
 });
